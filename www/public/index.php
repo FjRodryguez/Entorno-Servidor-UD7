02915ff2e5d9286ec34f1ad53d311571b2349914
Rafa Centeno <rgcenteno@edu.xunta.gal>
2025-01-15 16:42:28 +0100

    Base UD7 Manejo de excepciones

--- www/public/index.php
+++ www/public/index.php
@@ -2,14 +2,18 @@
 
 require '../vendor/autoload.php';
 mb_internal_encoding('UTF-8');
-try {
+try { //Otra opción con un manejador de excepciones: https://stackoverflow.com/questions/15245184/log-caught-exception-with-stack-trace
     $dotenv = Dotenv\Dotenv::createImmutable('../');
     $dotenv->load();
     Com\Daw2\Core\FrontController::main();
-} catch (Exception $e) {
+} catch (\Exception $e) {
+    http_response_code(500);
+    error_log($e);
     if ($_ENV['app.debug']) {
-        throw $e;
-    } else {
-        echo $e->getMessage();
+        echo json_encode(
+            [
+                'message' => $e->getMessage(),
+                'trace'   => $e->getTraceAsString()
+            ]);
     }
 }
